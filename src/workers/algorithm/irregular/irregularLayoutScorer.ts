0f71ed7d423f75b29e59c47feb7586e87bb4b1a7
jfet97 <andrysimo1997@gmail.com>
2026-07-19 08:05:05 +0200

    Test contact motif then intrinsic envelope

--- src/workers/algorithm/irregular/irregularLayoutScorer.ts
+++ src/workers/algorithm/irregular/irregularLayoutScorer.ts
@@ -514,12 +514,12 @@ export const STRUCTURAL_CONTACT_COUNT_BAND_WIDTH = 2
 
 const strictLayoutScoreOrder: Order.Order<IrregularLayoutScore> = Order.combineAll([
   scoreCriterion((score) => score.unplacedCount),
+  descendingScoreCriterion((score) => score.dominantNearCompleteStructuralContactCount),
+  descendingScoreCriterion((score) => score.nearCompleteStructuralContactCount),
   scoreCriterion(collisionBoundsMaxSideMm),
   scoreCriterion((score) => score.collisionBoundsAreaMm2),
   scoreCriterion((score) => score.collisionBoundsSpanMm),
   scoreCriterion((score) => score.occupiedHullWasteRatio),
-  descendingScoreCriterion((score) => score.dominantNearCompleteStructuralContactCount),
-  descendingScoreCriterion((score) => score.nearCompleteStructuralContactCount),
   descendingScoreCriterion((score) => score.sharedCollisionBoundaryContactBand),
   descendingScoreCriterion((score) => score.sharedCollisionBoundaryContactUnits),
   descendingScoreCriterion((score) => score.sharedCollisionBoundaryLengthMm),
